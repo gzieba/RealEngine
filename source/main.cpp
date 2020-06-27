c82013eb10c268bb53ac1fe504d37d06d781cf08
Gniewomir Zieba <gniewomir.zieba@gmail.com>
2020-06-27 22:56:23 +0200

    Adding submodules.

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,11 @@
-#include "Ui/MainWindow.h"
 #include "Common/logging.h"
 
 #include "RealEngine.h"
 
 INITIALIZE_EASYLOGGINGPP
 
-#include <QApplication>
-
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	MainWindow w;
-	w.show();
 	RealEngine r;
-	return a.exec();
+	return 0;
 }
